9c6d5695a92a4ea7d613062156c9ed556c223975
Oleksandr Myronchuk <smironcuk35@gmail.com>
2023-08-04 23:41:23 +0300

    fixes bugs

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log(process.env.API_URL, process.env.REACT_APP_VERCEL_TEST);
+
 export const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL || process.env.API_URL,
 });
