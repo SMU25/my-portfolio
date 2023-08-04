ff862d6e85af353fcef4ab3645867ef95b70bc55
Oleksandr Myronchuk <smironcuk35@gmail.com>
2023-08-04 23:15:29 +0300

    fixes bugs

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-// export const instance = axios.create({
-//   baseURL: "https://6399db1716b0fdad774bc5cf.mockapi.io",
-// });
-
 export const instance = axios.create({
-  baseURL: "https://sda213123",
+  baseURL: "https://6399db1716b0fdad774bc5cf.mockapi.io",
 });
